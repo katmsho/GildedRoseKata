e20aa5a5a67018021aad54369137f51db12570e5
katsho <katsho@katsho.com>
2025-06-14 18:35:58 +0100

    Moving special case handling to own functions to improve readability of for loop

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -29,14 +29,7 @@ public class GildedRose
             //increases in quality +1 before and on SellIn, +2 after sellIn
             if (Items[i].Name == "Aged Brie")
             {
-                if (Items[i].SellIn >= 0)
-                {
-                    Items[i].Quality = Math.Clamp(Items[i].Quality + 1, 0, 50);
-                }
-                else
-                {
-                    Items[i].Quality = Math.Clamp(Items[i].Quality + 2, 0, 50);
-                }
+                HandleAgedBrie(Items[i]);
 
                 continue;
             }
@@ -46,29 +39,9 @@ public class GildedRose
             //Quality increases by 2 when there are 10 days or less 
             //Quality increases by 3 when there are 5 days or less 
             //Quality drops to 0 after the concert
-
             if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (Items[i].SellIn >= 0)
-                {
-                    if (Items[i].SellIn >= 10)
-                    {
-                        Items[i].Quality = Math.Clamp(Items[i].Quality + 1, 0, 50);
-                    }
-                    if (Items[i].SellIn < 10 && Items[i].SellIn >= 5)
-                    {
-                        Items[i].Quality = Math.Clamp(Items[i].Quality + 2, 0, 50);
-                    }
-                    if (Items[i].SellIn < 5)
-                    {
-                        Items[i].Quality = Math.Clamp(Items[i].Quality + 3, 0, 50);
-                    }
-                }
-                else //is after SellIn
-                {
-                    Items[i].Quality = 0;
-                }
-
+                HandleBackstagePass(Items[i]);
                 continue;
             }
 
@@ -83,5 +56,40 @@ public class GildedRose
             }
 
         }
-    }    
+    }
+
+    private void HandleAgedBrie(Item item)
+    {
+        if (item.SellIn >= 0)
+        {
+            item.Quality = Math.Clamp(item.Quality + 1, 0, 50);
+        }
+        else
+        {
+            item.Quality = Math.Clamp(item.Quality + 2, 0, 50);
+        }
+    }
+
+    private void HandleBackstagePass(Item item)
+    { 
+        if (item.SellIn >= 0)
+        {
+            if (item.SellIn >= 10)
+            {
+                item.Quality = Math.Clamp(item.Quality + 1, 0, 50);
+            }
+            if (item.SellIn < 10 && item.SellIn >= 5)
+            {
+                item.Quality = Math.Clamp(item.Quality + 2, 0, 50);
+            }
+            if (item.SellIn < 5)
+            {
+                item.Quality = Math.Clamp(item.Quality + 3, 0, 50);
+            }
+        }
+        else //is after SellIn
+        {
+            item.Quality = 0;
+        }
+    }
 }
