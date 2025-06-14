13ea625c22973cc7a6f7f10dbd887ff928044ee8
katsho <katsho@katsho.com>
2025-06-14 20:41:16 +0100

    minor whitespace / formatting / comment updates

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -3,44 +3,32 @@ using System.Collections.Generic;
 
 namespace GildedRoseKata;
 
-public class GildedRose
-{
+public class GildedRose {
     public IList<Item> Items;
 
-    public GildedRose(IList<Item> Items)
-    {
+    public GildedRose(IList<Item> Items) {
         this.Items = Items;
     }
 
-    public void UpdateQuality()
-    {
+    public void UpdateQuality() {
+        foreach (Item item in this.Items) {
 
-        foreach (Item item in Items)
-        {
             //SPECIAL CASE Sulfuras
             //if Sulfuras, no change 
-            if (item.Name.StartsWith("Sulfuras"))
-            {
+            if (item.Name.StartsWith("Sulfuras")) {
                 continue;
             }
 
             item.SellIn = item.SellIn - 1;
 
             //SPECIAL CASE Aged Brie
-            //increases in quality +1 before and on SellIn, +2 after sellIn
-            if (item.Name.StartsWith("Aged Brie"))
-            {
+            if (item.Name.StartsWith("Aged Brie")) {
                 HandleAgedBrie(item);
                 continue;
             }
 
             //SPECIAL CASE Backstage pass
-            //Quality increases by as its SellIn value approaches
-            //Quality increases by 2 when there are 10 days or less 
-            //Quality increases by 3 when there are 5 days or less 
-            //Quality drops to 0 after the concert
-            if (item.Name.StartsWith("Backstage passes"))
-            {
+            if (item.Name.StartsWith("Backstage passes")) {
                 HandleBackstagePass(item);
                 continue;
             }
@@ -50,49 +38,50 @@ public class GildedRose
         }
     }
 
-    private void HandleAgedBrie(Item item)
-    {
-        if (item.SellIn >= 0)
-        {
+    /// <summary>
+    /// increases in quality +1 before and on SellIn, +2 after sellIn
+    /// </summary>
+    private void HandleAgedBrie(Item item) {
+        if (item.SellIn >= 0) {
             item.Quality = Math.Clamp(item.Quality + 1, 0, 50);
         }
-        else
-        {
+        else {
             item.Quality = Math.Clamp(item.Quality + 2, 0, 50);
         }
     }
 
-    private void HandleBackstagePass(Item item)
-    {
-        if (item.SellIn >= 0)
-        {
-            if (item.SellIn >= 10)
-            {
+    /// <summary>
+    ///Quality increases by 1 as its SellIn value approaches
+    ///Quality increases by 2 when there are 10 days or less 
+    ///Quality increases by 3 when there are 5 days or less 
+    ///Quality drops to 0 after SellIn
+    /// </summary>
+    private void HandleBackstagePass(Item item) {
+        if (item.SellIn >= 0) {
+            if (item.SellIn >= 10) {
                 item.Quality = Math.Clamp(item.Quality + 1, 0, 50);
             }
-            if (item.SellIn < 10 && item.SellIn >= 5)
-            {
+            if (item.SellIn < 10 && item.SellIn >= 5) {
                 item.Quality = Math.Clamp(item.Quality + 2, 0, 50);
             }
-            if (item.SellIn < 5)
-            {
+            if (item.SellIn < 5) {
                 item.Quality = Math.Clamp(item.Quality + 3, 0, 50);
             }
         }
-        else
-        { //is after SellIn
+        else { //is after SellIn
             item.Quality = 0;
         }
     }
 
-    private void HandleStandardItem(Item item)
-    {
-        if (item.SellIn >= 0)
-        {
+    /// <summary>
+    ///Quality decreases by 1 as its SellIn value approaches
+    ///Quality decreases by 1 after SellIn
+    /// </summary>
+    private void HandleStandardItem(Item item) {
+        if (item.SellIn >= 0) {
             item.Quality = Math.Clamp(item.Quality - 1, 0, 50);
         }
-        else
-        {
+        else {
             item.Quality = Math.Clamp(item.Quality - 2, 0, 50);
         }
     }
